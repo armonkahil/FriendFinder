fe203e2a677180a65c0283718e823afaf63144f9
South Matic <Southmatic729@gmail.com>
2019-10-31 21:17:02 -0400

    another update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,3 +1,11 @@
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, '/public/home.html'))
-})
+const path = require('path')
+
+module.exports = function (app) {
+  app.get('/survey', function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/survey.html'))
+  })
+
+  app.use(function (req, res) {
+    res.sendFile(path.join(__dirname, '../public/home.html'))
+  })
+}
---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+var app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+
+require('./app/routing/apiRoutes.js')(app)
+require('./app/routing/htmlRoutes.js')(app)
+
+var PORT = process.env.PORT || 3000
+
+app.listen(PORT, function () {
+  console.log('App listening on Port ' + PORT)
+})
